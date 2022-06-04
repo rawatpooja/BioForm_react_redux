b7935197f6b5d44e152c5b786adfa982683aa3c7
rawatpooja <90598413+rawatpooja@users.noreply.github.com>
2022-06-04 19:57:03 +0530

    Add files via upload

--- bioinformation.js
+++ bioinformation.js
@@ -1,2 +1,12 @@
-export const addData =(payload)=>({type:'ADD_DATA',payload:payload})
+const initState ={
+    list:[],
+};
 
+const bioinformation = (state = initState , action)=>{
+if(action.type ==='ADD_DATA'){
+    return{...state,list:[...state.list,action.payload]}
+} else{
+    return state;
+}
+}
+export default bioinformation;
